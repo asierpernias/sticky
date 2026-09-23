229f0aa9ab52f4a66828c87f2dc04507460ba6ac
APSwinner <ague_soria@yahoo.es>
2026-09-23 18:33:10 +0200

    shadow/ border logic

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const fileButton = document.getElementById('fileUpload');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let currentShape = 'square';
+let borderThickness = 0;
+let borderRadius = 0; 
+let borderColor = '#000000';
+
+let shadowX = 0;
+let shadowY = 0;
+let shadowBlur = 0;
+let shadowOpacity = 0.8;
+let shadowColorValue = '#000000';
+
 let currentImg = null;
 let zoom = 1;
 let offsetX = 0;
@@ -15,9 +26,6 @@ document.getElementById('UploadButton').addEventListener('click', () => {
     fileClick();
 });
 
-function fileClick(){
-    document.getElementById('fileUpload').click();
-}
 
 fileButton.addEventListener('change', (event) => {
     const file = event.target.files[0];
@@ -35,6 +43,33 @@ fileButton.addEventListener('change', (event) => {
     img.src = URL.createObjectURL(file);
 });
 
+function tracePath(x, y, w, h, shape, radius) {
+    ctx.beginPath();
+
+    if (shape === 'circle'){
+        const r = Math.min(w, h) / 2;
+        ctx.arc(x + w / 2, y + h / 2, r, 0, Math.PI * 2);
+    } else if (shape === 'rounded'){
+        ctx.roundRect(x, y, w, h, radius)
+    } else if (shape === 'hexagon'){
+        const cx = x + w /2;
+        const cy = y + h /2;
+        const r = Math.min(w, h) / 2;
+        for (let i = 0; i < 6; i++){
+            const angle = (Math.PI / 3) * 1 - Math.PI / 2;
+            const px = cx + r * Math.cos(angle);
+            const py = cy + r * Math.sin(angle);
+            if (i === 0) ctx.moveTo(px, py);
+            else ctx.moveTo(px, py)
+        } 
+    } else {
+        ctx.rect(x, y, w, h);
+    }
+}
+
+function fileClick(){
+    document.getElementById('fileUpload').click();
+}
 
 function drawCanvasImgae(){
     if (!currentImg) return;
@@ -45,15 +80,35 @@ function drawCanvasImgae(){
 
     let escalaFinal = escala * zoom;
 
-    const anchoFinal = currentImg.width * escalaFinal;
-    const altoFinal = currentImg.height * escalaFinal;
+    const borderX = imgX - borderThickness;
+    const borderY = imgY - borderThickness;
+    const borderW = imgW + 2 * borderThickness;
+    const borderH = imgH + 2 * borderThickness;
 
-    const x = (canvas.width - anchoFinal) / 2 + offsetX;
-    const y = (canvas.height - altoFinal) / 2 + offsetY;
-    
+    const imgW = currentImg.width * escalaFinal;
+    const imgH = currentImg.height * escalaFinal;
+    const imgX = (canvas.width - imgW) / 2 + offsetX;
+    const imgY = (canvas.height - imgH) / 2 + offsetY;
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(currentImg, x, y, anchoFinal, altoFinal);
+    if (borderThickness > 0){
+        ctx.save();
+
+        ctx.shadowColor = hexToRgba(shadowColorValue, shadowOpacity);
+        ctx.shadowBlur = shadowBlur;
+        ctx.shadowOffsetX = shadowX;
+        ctx.shadowOffsetY = shadowY;
+
+        tracePath(borderX, borderY, borderW, borderH,  currentShape, borderRadius);
+        ctx.fillStyle = borderColor;
+        ctx.fill();
+
+        ctx.restore();
+    }
+
+    ctx.save();
+    tracePath(imgX, imgY, imgW, imgH, currentShape, borderRadius);
+    ctx.clip();
+    ctx.drawImage(currentImg, imgX, imgY, imgW, imgH);
 }
 
 canvas.addEventListener('wheel', (event) => {
@@ -98,3 +153,10 @@ canvas.addEventListener('mouseup', () => {
 canvas.addEventListener('mouseleave', () => {
     isDragging = false;
 });
+
+function hexToRgba(hex, opacityPercent){
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5,7), 16);
+    return `rgba(${r}, ${g}, ${b}, ${opacityPercent / 100})`;
+}
